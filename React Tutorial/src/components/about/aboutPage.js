f4e7ba2a2a9ac9e0903dc09dfeae430ae9dd817d
Noah Cardell <noahc@aluminumtrailer.com>
2017-01-04 14:26:45 -0500

    Completed React Lifecycle Module

---
+++ React Tutorial/src/components/about/aboutPage.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+  render: function () {
+    return (
+      <div className="jumbotron">
+        <h1>About</h1>
+        <p>
+          This Application used the following technologies
+          <ul>
+            <li>React</li>
+            <li>React Router</li>
+            <li>Flux</li>
+            <li>Node</li>
+            <li>Gulp</li>
+            <li>Browserify</li>
+            <li>Bootstrap</li>
+          </ul>
+        </p>
+      </div>
+    );
+  }
+});
+
+module.exports = About;
